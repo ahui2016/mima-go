588ddf096a43e9d6abef577fe3a0bc3e82e75815
ahui <ahui.2015@icloud.com>
2020-02-05 23:52:05 +0800

    TestQiniu_Upload 测试成功

--- qiniu/qiniu.go
+++ qiniu/qiniu.go
@@ -1,23 +1,77 @@
 package qiniu
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/qiniu/api.v7/v7/auth"
 	"github.com/qiniu/api.v7/v7/storage"
+	"path/filepath"
+)
+
+var (
+	ErrBlankUpToken = errors.New("未生成 upToken")
 )
 
 type Qiniu struct {
-	accessKey string
-	secretKey string
-	bucket    string
+	accessKey      string
+	secretKey      string
+	bucket         string
+	folder         string
+	zone           *storage.Region
+	upToken        string
+	KeyToOverwrite string
 }
 
-func NewQiniu(accessKey, secretKey, bucket string) *Qiniu {
-	return &Qiniu{accessKey, secretKey, bucket}
+func NewQiniu(accessKey, secretKey, bucket, folder string, zone *storage.Region) *Qiniu {
+	return &Qiniu{
+		accessKey: accessKey,
+		secretKey: secretKey,
+		bucket:    bucket,
+		folder:    folder,
+		zone:      zone,
+	}
 }
 
-func (qn *Qiniu) GetUpToken() string {
-	putPolicy := storage.PutPolicy{Scope: qn.bucket}
-	return putPolicy.UploadToken(
+func (qn *Qiniu) createUpToken() {
+	putPolicy := storage.PutPolicy{}
+	if qn.KeyToOverwrite == "" {
+		putPolicy.Scope = qn.bucket
+	} else {
+		putPolicy.Scope = fmt.Sprintf("%s:%s", qn.bucket, qn.KeyToOverwrite)
+	}
+	qn.upToken = putPolicy.UploadToken(
 		auth.New(qn.accessKey, qn.secretKey),
 	)
+}
+
+func (qn *Qiniu) formUpload(localFile string) (ret storage.PutRet, err error) {
+	cfg := storage.Config{
+		Zone:     qn.zone,
+		UseHTTPS: false,
+	}
+	formUploader := storage.NewFormUploader(&cfg)
+	err = formUploader.PutFile(
+		context.Background(),
+		&ret,
+		qn.upToken,
+		fmt.Sprintf("%s/%s", qn.folder, filepath.Base(localFile)),
+		localFile,
+		nil,
+	)
+	return
+}
+
+// Upload 需要先获取 upToken, 而在获取 upToken 之前要先设置 KeyToOverwrite.
+// TODO: 根据错误信息, 重新获取 upToken.
+func (qn *Qiniu) Upload(localFile string, overwrite bool) (ret storage.PutRet, err error) {
+	if overwrite {
+		qn.KeyToOverwrite = fmt.Sprintf("%s/%s", qn.folder, filepath.Base(localFile))
+	} else {
+		qn.KeyToOverwrite = ""
+	}
+	if qn.upToken == "" {
+		qn.createUpToken()
+	}
+	return qn.formUpload(localFile)
 }
--- qiniu/qiniu_test.go
+++ qiniu/qiniu_test.go
@@ -1,13 +1,28 @@
 package qiniu
 
-import "testing"
+import (
+	"github.com/qiniu/api.v7/v7/storage"
+	"testing"
+)
 
 var qn = NewQiniu(
 	"TvFfavKeGJbsSyOxMOcOweGCqyb4b_ghdbuUjYKL",
 	"fPHLopOXEwyT2E62rK_6nPpRu0RbwU7seI8DMbae",
 	"er0er0",
+	"mima-go/temp",
+	&storage.ZoneHuanan,
 )
 
-func TestQiniu_GetUpToken(t *testing.T) {
-	t.Log(qn.GetUpToken())
+func TestQiniu_createUpToken(t *testing.T) {
+	t.Skip("一般情况下在 TestQiniu_Upload 中已包含本测试")
+	qn.createUpToken()
+	t.Log(qn.upToken)
+}
+
+func TestQiniu_Upload(t *testing.T) {
+	ret, err := qn.Upload("qiniu.go", true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(ret)
 }
